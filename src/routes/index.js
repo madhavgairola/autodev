04274ad2b477b08f91503dc4910d1d2b78b6c886
Madhav Gairola <madhavgairola05@gmail.com>
2026-06-05 21:01:41 +0530

    day 2 updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const port = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.status(200).json("Working");
+    return res.status(200).json({ status: "ok" });
 })
-
+app.get("/api/v1/test", (req, res) => {
+    return res.status(200).json({ status: "ok", message: "AutoDev API running" });
+});
 app.listen(port, () => console.log(`Server is Running on Port ${port}`));
 
